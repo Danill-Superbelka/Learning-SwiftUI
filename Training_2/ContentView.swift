a888af561a3670827b17d42fac163c70ee5ba597
Daniil <51852490+Danill-Superbelka@users.noreply.github.com>
2021-07-17 14:08:04 +0300

    Initial commit

---
+++ Training_2/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  Training_2
+//
+//  Created by Даниил  on 17.07.2021.
+//
+
+import SwiftUI
+
+class User {
+    var firstName = "Bilbo"
+    var lastName = "Baqqins"
+}
+struct ContentView: View {
+    @State private var user = User()
+    
+    var body: some View {
+        VStack{
+            Text("Your name is \(user.firstName) \(user.lastName)")
+            
+            TextField("First name", text: $user.firstName)
+            TextField("Last name", text: $user.lastName)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
