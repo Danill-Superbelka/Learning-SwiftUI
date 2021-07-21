ca4727557481aba47b3685046c223826e70eb30d
Daniil <51852490+Danill-Superbelka@users.noreply.github.com>
2021-07-21 19:35:37 +0300

    more lessons
    Deleting items

---
+++ Delet_Update_Data/Delet_Update_Data/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Delet_Update_Data
+//
+//  Created by Даниил  on 21.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+    
+    var body: some View {
+        VStack{
+            List{
+                ForEach(numbers, id: \.self) {
+                    Text("\($0)")
+                }
+            }
+            Button("Add Number") {
+                self.numbers.append(self.currentNumber)
+                self.currentNumber += 1
+            }
+        }
+    }
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
