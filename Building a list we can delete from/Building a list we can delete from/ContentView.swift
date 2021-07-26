2073766aa40a208f53334c2923727f50633f41c3
Daniil <51852490+Danill-Superbelka@users.noreply.github.com>
2021-07-26 20:50:39 +0300

    Lesson 4

--- Building a list we can delete from/Building a list we can delete from/ContentView.swift
+++ Building a list we can delete from/Building a list we can delete from/ContentView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ExpenseItem: Identifiable {
+struct ExpenseItem: Identifiable, Codable {
     let id =  UUID()
     let name: String
     let type: String
@@ -19,9 +19,20 @@ class Expenses: ObservableObject {
         didSet {
             let encoder = JSONEncoder()
             if let encoded = try? encoder.encode(items) {
-                UserDefaults.standard.set(encoder, forKey: "Items")
+                UserDefaults.standard.set(encoded, forKey: "Items")
             }
         }
+    }
+    
+    init(){
+        if let items = UserDefaults.standard.data(forKey: "Items") {
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode([ExpenseItem].self, from: items) {
+                self.items = decoded
+                return
+            }
+        }
+        self.items = []
     }
 }
 
