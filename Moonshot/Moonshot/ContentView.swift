64c227936621bc7742e4b83ff5081f3e8bb6dd3f
Daniil <51852490+Danill-Superbelka@users.noreply.github.com>
2021-07-27 21:39:08 +0300

    Moonshot

---
+++ Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Даниил  on 27.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image("nature")
+                .frame(width: 300, height: 300)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
