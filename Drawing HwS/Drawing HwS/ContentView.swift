2cdf60c1221baad2de00be0dde642e858d96a0cf
Daniil <51852490+Danill-Superbelka@users.noreply.github.com>
2021-07-31 13:19:18 +0300

    Drawing Lesson

---
+++ Drawing HwS/Drawing HwS/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  Drawing HwS
+//
+//  Created by Даниил  on 31.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Path { path in
+            path.move(to: CGPoint(x: 200, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 300))
+            path.addLine(to: CGPoint(x: 300, y: 300))
+            path.addLine(to: CGPoint(x: 200, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 300))
+            
+        }
+        .stroke(Color.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
