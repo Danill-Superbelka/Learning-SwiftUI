84243a50dbd99324b15fb944b1982e4ce83258bd
Daniil <51852490+Danill-Superbelka@users.noreply.github.com>
2021-08-04 13:25:24 +0300

    Cupcake_Final

--- CupcakeApp/CupcakeApp/Checkout.swift
+++ CupcakeApp/CupcakeApp/Checkout.swift
@@ -9,6 +9,39 @@ import SwiftUI
 
 struct Checkout: View {
     @ObservedObject var order: Order
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    
+    func placeOrder(){
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = encoded
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                print("No data in response: \(error?.localizedDescription ?? "Unknown error").")
+                return
+            }
+            
+            if let decoderOrder = try? JSONDecoder().decode(Order.self, from: data) {
+                self.confirmationMessage = "Your order for \(decoderOrder.quantity)x \(Order.types[decoderOrder.type].lowercased()) cupcakes is on its way!"
+                self.showingConfirmation = true
+            } else {
+                print("Invalid response from server")
+            }
+        }.resume()
+        
+        
+    }
+    
     var body: some View {
         GeometryReader { geo in
             ScrollView {
@@ -22,13 +55,16 @@ struct Checkout: View {
                         .font(.title)
                     
                     Button("Place Order") {
-                        //
+                        self.placeOrder()
                     }
                     .padding()
                 }
             }
         }
         .navigationBarTitle("Check out", displayMode: .inline)
+        .alert(isPresented: $showingConfirmation) {
+            Alert(title: Text("Thank you!"), message: Text(confirmationMessage), dismissButton: .default(Text("Ok")))
+        }
     }
 }
 
