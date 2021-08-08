06502f62f365ff449675ad1567d592aa04470b0a
Daniil <51852490+Danill-Superbelka@users.noreply.github.com>
2021-08-08 18:41:50 +0300

    BookWorm

--- BookwormApp/BookwormApp/ContentView.swift
+++ BookwormApp/BookwormApp/ContentView.swift
@@ -9,33 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(entity: Student.entity(), sortDescriptors: []) var students: FetchedResults<Student>
+    @FetchRequest(entity: Book.entity(), sortDescriptors: []) var books: FetchedResults<Book>
+
+    @State private var showingAddScreen = false
+    @State private var title = ""
+    @State private var author = ""
+    @State private var rating = 3
+    @State private var genre = ""
+    @State private var review = ""
+    
+    let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
+    
     
     var body: some View {
-        VStack{
-            List{
-                ForEach(students, id: \.id) { students in
-                    Text(students.name ?? "NoN")
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Name of book", text: $title)
+                    TextField("Author`s name", text: $author)
                     
+                    Picker("Genre", selection: $genre){
+                        ForEach(genres, id: \.self){
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Section{
+                    Picker("Rating", selection: $rating){
+                        ForEach(0..<6){
+                            Text("\($0)")
+                        }
+                    }
+                    TextField("Write a review", text: $review)
+                }
+                
+                Section{
+                    Button("Save"){
+                       let newBook =
+                    }
                 }
             }
-            Button("Add") {
-                let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
-                let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
-                
-                let chosenFirstName = firstNames.randomElement()!
-                let chosenLastName = lastNames.randomElement()!
-                
-                
-                let student = Student(context: self.moc)
-                student.id = UUID()
-                student.name = "\(chosenFirstName) \(chosenLastName)"
-                
-                try? self.moc.save()
-            }
+            .navigationBarTitle("AddBook")
         }
     }
 }
+    
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
