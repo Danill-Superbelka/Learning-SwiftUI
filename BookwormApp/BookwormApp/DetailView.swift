f46eba574b31e27aaed35f17e35080c98cf4373c
Daniil <51852490+Danill-Superbelka@users.noreply.github.com>
2021-08-10 14:31:14 +0300

    Bookworm_DetailView

---
+++ BookwormApp/BookwormApp/DetailView.swift
@@ -0,0 +1,65 @@
+//
+//  DetailView.swift
+//  BookwormApp
+//
+//  Created by Даниил  on 10.08.2021.
+//
+
+import SwiftUI
+import CoreData
+
+struct DetailView: View {
+    
+    let book: Book
+    
+    var body: some View {
+        GeometryReader { geometry in
+            VStack{
+                ZStack(alignment: .bottomTrailing) {
+                    Image(self.book.genre ?? "Fantasy")
+                        .frame(maxWidth: geometry.size.width)
+                    
+                    Text(self.book.genre?.uppercased() ?? "FANTASY")
+                        .font(.caption)
+                        .fontWeight(.black)
+                        .padding(8)
+                        .foregroundColor(.white)
+                        .background(Color.black.opacity(0.75))
+                        .clipShape(Capsule())
+                        .offset(x: -5, y: -5)
+                }
+            }
+            
+            Text(self.book.author ?? "Unknown author")
+                .font(.title)
+                .foregroundColor(.secondary)
+            
+            Text(self.book.review ?? "No review")
+                .padding()
+            
+            RatingView(rating: .constant(Int(self.book.rating)))
+                .font(.largeTitle)
+            
+            Spacer()
+        }
+        .navigationBarTitle(Text(book.title ?? "Unknown book"), displayMode: .inline)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    
+    static let moc = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+    
+    static var previews: some View {
+        let book = Book(context: moc)
+        book.title = "Test Book"
+        book.author = "Test Author"
+        book.genre = "Fantasy"
+        book.rating = 4
+        book.review = "The looooong text here"
+        
+        return NavigationView{
+            DetailView(book: book)
+        }
+    }
+}
