d507a68ce03937e907f275f18b070233e942adeb
Daniil <51852490+Danill-Superbelka@users.noreply.github.com>
2021-08-12 15:38:42 +0300

    Instafilter_binding

--- Insatfilter/Insatfilter/ContentView.swift
+++ Insatfilter/Insatfilter/ContentView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var blurAmount: CGFloat = 0 {
+        didSet{
+            print("New value is \(blurAmount)")
+        }
+    }
+    
     var body: some View {
+        let blur = Binding<CGFloat>(
+            get: {
+                self.blurAmount
+            },
+            set: {
+                self.blurAmount = $0
+                print("New value is \(self.blurAmount)")
+            }
+        )
+        
+        return VStack{
         Text("Hello, world!")
-            .padding()
+            .blur(radius: blurAmount)
+            
+            Slider(value: blur, in: 0...20)
+        }
     }
 }
 
