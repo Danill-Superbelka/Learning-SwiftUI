3721a47d7d94b8fa3cb5d6fd926c64f979ed6d62
Daniil <51852490+Danill-Superbelka@users.noreply.github.com>
2021-08-14 11:39:05 +0300

    Instafilter

--- Insatfilter/Insatfilter/ContentView.swift
+++ Insatfilter/Insatfilter/ContentView.swift
@@ -9,45 +9,14 @@ import SwiftUI
 import CoreImage
 import CoreImage.CIFilterBuiltins
 
-class ImageSaver: NSObject {
-    func writeToPhotoAlbum(image: UIImage) {
-        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
-    }
-    
-    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
-            print("Save finished!")
-        }
-}
-
 struct ContentView: View {
     
-    @State private var image: Image?
-    @State private var inputImage: UIImage?
-    @State private var showingImagePicker = false
-    
     var body: some View {
-        VStack{
-            image?
-                .resizable()
-                .scaledToFit()
-            
-            Button("SelectImage") {
-                self.showingImagePicker = true
-            }
-        }
-        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
-            ImagePicker(image: self.$inputImage)
+        Text("hello world")
+            .padding()
         }
     }
     
-    func loadImage() {
-        guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
-        let imageSaver = ImageSaver()
-        imageSaver.writeToPhotoAlbum(image: inputImage)
-        
-    }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
