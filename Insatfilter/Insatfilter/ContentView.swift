4303bc93c1eda393580653c638412552c7cb439e
Daniil <51852490+Danill-Superbelka@users.noreply.github.com>
2021-08-16 17:06:04 +0300

    InstaFilter

--- Insatfilter/Insatfilter/ContentView.swift
+++ Insatfilter/Insatfilter/ContentView.swift
@@ -9,15 +9,30 @@ import SwiftUI
 import CoreImage
 import CoreImage.CIFilterBuiltins
 
+
 struct ContentView: View {
     
     @State private var image: Image?
     @State private var filterIntensity = 0.5
     @State private var showingImagePicker = false
     @State private var inputImage: UIImage?
+    @State private var currentFilter = CIFilter.sepiaTone()
+    
+    let context = CIContext()
     
     var body: some View {
-        NavigationView {
+        
+        let intensity = Binding<Double> (
+            get: {
+                self.filterIntensity
+            },
+            set: {
+                self.filterIntensity = $0
+                self.applyProcessing()
+            }
+        )
+        
+      return  NavigationView {
             VStack {
                 ZStack {
                     Rectangle()
@@ -39,7 +54,7 @@ struct ContentView: View {
                 
                 HStack{
                     Text("Intensity")
-                    Slider(value: self.$filterIntensity)
+                    Slider(value: intensity)
                 }.padding(.vertical)
                 
                 HStack{
@@ -65,9 +80,24 @@ struct ContentView: View {
     
     func loadImage(){
         guard let inputImage = inputImage else {return}
-        image = Image(uiImage: inputImage)
+        
+        let beginImage = CIImage(image: inputImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
     }
+        
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+        
+        guard let outputImage = currentFilter.outputImage else {return}
+        
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+        }
     }
+    
+}
     
 
 struct ContentView_Previews: PreviewProvider {
